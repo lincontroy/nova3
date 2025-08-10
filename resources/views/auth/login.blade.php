a34a6c384792ab45c44fdf6d7caba829e187d7f3
lincontroy <lincolnmunene37@gmail.com>
2025-08-10 17:12:39 +0300

    updates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
         <!-- /.login-logo -->
         <div class="card card-outline card-primary">
             <div class="card-header text-center">
-                <a href="/" class="h1"><b>Nova Investments</a>
+                <a href="/" class="h1"><b>Nova Advertisement</a>
             </div>
             <div class="card-body">
                 <p class="login-box-msg">Sign in to start your session</p>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="register-box">
         <div class="card card-outline card-primary">
             <div class="card-header text-center">
-                <a href="/" class="h1"><b>Nova investments</a>
+                <a href="/" class="h1"><b>Nova Advertisement</a>
             </div>
             <div class="card-body">
                 <p class="login-box-msg">Register a new membership</p>
